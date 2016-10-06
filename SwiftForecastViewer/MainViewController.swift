487fdf2497ca18935f82dc33bab91bac4e8234e3
M.A. Castro <castro.michael87@gmail.com>
2016-10-06 12:52:34 -0500

    MainVC and Settings VC in Storyboard and Controllers

---
+++ SwiftForecastViewer/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  SwiftForecastViewer
+//
+//  Created by Michael Castro on 10/5/16.
+//  Copyright © 2016 Michael Castro. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let reuseIdentifier = "MainCell"
+    weak var mainCell : UICollectionViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension MainViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+
+}
---
+++ SwiftForecastViewer/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  SwiftForecastViewer
+//
+//  Created by Michael Castro on 10/5/16.
+//  Copyright © 2016 Michael Castro. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
